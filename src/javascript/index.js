52f83cd47e1ec5bf0c900cdede476235713af722
Ramin Mammadzada <remki-design@Hotmail.com>
2020-12-17 22:46:22 +0300

    Add webpack config, init npm dependencies and structure files.

---
+++ src/javascript/index.js
@@ -0,0 +1,14 @@
+import '../css/styles.css';
+// import img1 from '../images/img2.jpeg'
+console.log("hi all");
+
+const h1  = document.createElement("h1");
+h1.textContent = "miami";
+
+const body = document.getElementsByTagName("body")[0];
+body.appendChild(h1);
+
+// const imgTag = document.createElement("img");
+// imgTag.setAttribute("src", img1);
+
+// body.appendChild(imgTag);
